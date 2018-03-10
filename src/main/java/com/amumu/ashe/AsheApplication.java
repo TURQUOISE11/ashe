422aa5e3c3d226a7d2102e720a3f1c71b9a5b88c
TURQUOISE <275776819@qq.com>
2018-03-10 15:41:16 +0800

    Initial commit

---
+++ src/main/java/com/amumu/ashe/AsheApplication.java
@@ -0,0 +1,12 @@
+package com.amumu.ashe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsheApplication.class, args);
+	}
+}
