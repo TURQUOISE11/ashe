c0daac9a3ee002061b1e54e68ccc7054c8683f25
TURQUOISE <275776819@qq.com>
2018-03-10 16:43:08 +0800

    创建项目

---
+++ src/main/java/com/amumu/ashe/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.amumu.ashe.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class TestController {
+    @RequestMapping("/world")
+    public String hello(){
+        return "hello";
+    }
+}
